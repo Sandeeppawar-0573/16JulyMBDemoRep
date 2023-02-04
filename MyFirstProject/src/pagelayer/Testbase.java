55b42dabe8d6947a2c2d3aa23ba9000173990c62
sandeep pawar <sandeeppawar0573@gmail.com>
2023-02-05 00:32:29 +0530

    This is my new Project
    Hi all,
    I have completed my task go ahed and clone it.

---
+++ MyFirstProject/src/pagelayer/Testbase.java
@@ -0,0 +1,12 @@
+package pagelayer;
+
+public class Testbase {
+
+	public static void main(String[] args) {
+		System.out.println("Congrats ");
+		System.out.println("Create new Project");
+		System.out.println("Create new  package and class");
+
+	}
+
+}
